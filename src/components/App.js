d457621ad3af146f26e973ef2eb8f139f1ac2b28
Yashvant <prajapatiyashvant746@gmail.com>
2022-12-14 15:29:50 +0530

    Intial commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1   data-ns-test ="project-name">The online shopping mall</h1>
+      <p  data-ns-test ="project-description" >Here you can buy every collection of any things with very local market price.It will modern mall website with sample video product you can know pros and cons before purching anythings </p>
+    </div>
+
   )
 }
 
